d6b47c35305265173ff0828c91d0858b39579968
Valeria <valeria.stamenova3@gmail.com>
2021-01-30 11:57:57 +0200

    Event.cs Creation (not finished)
    Probably will need more corrections or functionality

---
+++ DIS-Journal/DIS-Journal/Event.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal
+{
+    class Event
+    {
+        private DateTime _dateTime;
+        private string _title;
+        private string _description;
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return this._dateTime;
+            }
+            set
+            {
+                this._dateTime = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this._title;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("The title of the event cannot be empty");
+                }
+                else
+                {
+                    this._title = value;
+                }
+            }
+        }
+
+        public string Description //optional IMOO
+        {
+            get
+            {
+                return this.Description;
+            }
+            set
+            {
+                this._description = value;
+            }
+        }
+
+        public Event(DateTime dateTime, string title, string description)
+        {
+            this.DateTime = dateTime;
+            this.Title = title;
+            this.Description = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{this._title}";
+        }
+    }
+}
