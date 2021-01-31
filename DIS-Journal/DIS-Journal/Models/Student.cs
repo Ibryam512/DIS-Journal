4871004b68af15b7a752ff82e5197cd083f883c6
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-01-31 13:03:51 +0200

    created Student and Teacher classes

---
+++ DIS-Journal/DIS-Journal/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal.Models
+{
+    class Student : User
+    {
+        public Student(string firstName, string lastName, string email, string password, int age) : base(firstName, lastName, email, password, age, "student")
+        { }
+    }
+}
---
+++ DIS-Journal/DIS-Journal/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal.Models
+{
+    class Teacher : User
+    {
+        public Teacher(string firstName, string lastName, string email, string password, int age) : base(firstName, lastName, email, password, age, "teacher")
+        { }
+    }
+}
