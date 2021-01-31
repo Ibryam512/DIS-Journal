b39db5dc1774736d6ee6fc18addb29a4b739bc44
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-01-31 12:58:52 +0200

    created User class

---
+++ DIS-Journal/DIS-Journal/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal.Models
+{
+    class User : IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Role { get; set; }
+
+        public User(string firstName, string lastName, string email, string password, int age, string role = "user")
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.Password = password;
+            this.Age = age;
+            this.Role = role;
+        }
+       
+    }
+}
