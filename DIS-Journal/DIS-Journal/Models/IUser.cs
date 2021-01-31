b80c47a9cf1064bbc3f801b0aa3256ff9a58c49d
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-01-31 12:54:23 +0200

    created IUser interface

---
+++ DIS-Journal/DIS-Journal/Models/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal.Models
+{
+    interface IUser
+    {
+        string FirstName { get; }
+        string LastName { get; }
+        string Email { get;  }
+        string Password { get; }
+        int Age { get; }
+        string Role { get; }
+    }
+}
