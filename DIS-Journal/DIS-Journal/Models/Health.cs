1a8dc2cbdf79dc9afc793ba2f099eed4c87b2ae0
Niksss123 <78233918+Niksss123@users.noreply.github.com>
2021-02-02 13:04:36 +0200

    Routine

---
+++ DIS-Journal/DIS-Journal/Models/Health.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal.Models
+{
+    class Health
+    {
+        //private string exercices;
+        private List<Health> excercices;
+        private List<Health> pills;
+        private List<Health> meals; //eventualno da se razpredelqt po dni i vid???
+        //private string sleep;
+        private int hour1;  //falling asleep
+        private int hour2;  // waking up
+        private string description; 
+        private string dreams;  //idk man
+        private List<Health> other_activities;
+
+        public List<Health> Excercices
+        { get { return this.excercices; } }
+
+        public List<Health> Pills
+        { get { return this.pills; } }
+
+        public List<Health> Meals
+        { get { return this.meals; } }
+
+        public List<Health> Other_activities
+        { get { return this.other_activities; } }
+
+        public int Hour1
+        {
+            get { return this.hour1; }
+            set { this.hour1 = value; }
+        }
+
+        public int Hour2
+        {
+            get { return this.hour2; }
+            set { this.hour2 = value; }
+        }
+
+        public string Description
+        {
+            get { return this.description; }
+            set { this.description= value; }
+        }
+
+        public string Dreams
+        {
+            get { return this.dreams; }
+            set { this.dreams = value; }
+        }
+
+        public Health()
+        {
+            this.excercices = new List<Health>();
+            this.pills = new List<Health>();
+            this.meals = new List<Health>();
+            this.other_activities = new List<Health>();
+            this.Hour1 = hour1;
+            this.Hour2 = hour2;
+            this.Dreams = dreams;
+            this.Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"";
+        }
+    }
+}
---
+++ DIS-Journal/DIS-Journal/Models/Journal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal.Models
+{
+    class Journal
+    {
+        private string title;
+        private DateTime date;
+        private string description;
+
+        public string Title
+        {
+            get { return this.title; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("The title of the event cannot be empty");
+                }
+                else
+                {
+                    this.title = value;
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return this.date; }
+            set { this.date = value; }
+        }
+
+        public string Description
+        {
+            get { return this.description; }
+            set { this.description = value; }
+        }
+
+        public Journal(string title, DateTime date, string description)
+        {
+            this.Date = date;
+            this.Title = title;
+            this.Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"";
+        }
+
+
+    }
+}
