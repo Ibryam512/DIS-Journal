c8a66be14af95fedd774b6ebbc154314a87a9527
Valeria <valeria.stamenova3@gmail.com>
2021-02-03 16:58:51 +0200

    Add CalendarController.cs and rewrite Calendar.cs

---
+++ DIS-Journal/DIS-Journal/Controllers/CalendarController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIS_Journal.Models;
+
+namespace DIS_Journal.Controllers
+{
+    class CalendarController
+    {
+        public Calendar calendar;
+
+        public void AddEvent(Event e)
+        {
+            if (e.Repeatedness == Interval.Once || e.Repeatedness == Interval.Monthly || e.Repeatedness == Interval.Yearly)
+            {
+                calendar.Events.Add(e);
+            }
+        }
+
+        public void AddEvent(DateTime date, string title, string description, Interval period)
+        {
+            if (period == Interval.Once || period == Interval.Monthly || period == Interval.Yearly)
+            {                                                                                     
+                calendar.Events.Add(new Event(date, title, description, period));
+            }
+        }
+
+        public void RemoveEvent(Event e)
+        {
+            calendar.Events.Remove(e);
+        }
+
+        public void RemoveEvent(DateTime date, string title)
+        {
+            Event e = calendar.Events.Find(x => x.DateTime == date && x.Title == title);
+            RemoveEvent(e);
+        }
+    }
+}
--- DIS-Journal/DIS-Journal/Models/Calendar.cs
+++ DIS-Journal/DIS-Journal/Models/Calendar.cs
@@ -32,32 +32,5 @@ namespace DIS_Journal.Models
             this._events = new List<Event>();
             this._daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
         }
-
-        public void AddEvent(Event e)
-        {
-            if (e.Repeatedness == Interval.Once || e.Repeatedness == Interval.Monthly || e.Repeatedness == Interval.Yearly)
-            {
-                this._events.Add(e);
-            }
-        }
-
-        public void AddEvent(DateTime date, string title, string description, Interval period)
-        {
-            if(period == Interval.Once || period == Interval.Monthly || period == Interval.Yearly) //we'll remove them when we're doing the
-            {                                                                                     //input but for now let them be just to remember about it
-                this._events.Add(new Event(date, title, description, period));
-            }              
-        }
-
-        public void RemoveEvent(Event e)
-        {
-            this._events.Remove(e);
-        }
-
-        public void RemoveEvent(DateTime date, string title)
-        {
-            Event e = this._events.Find(x => x.DateTime == date && x.Title == title);
-            RemoveEvent(e);
-        }
     }
 }
