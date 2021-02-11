0e82dce06bb4ed4e1fd801824d1675b21a526022
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-02-11 17:53:31 +0200

    Created Logged.cs
    Logged.cs is a file in which will be saved temporally the information of the logged user when he logs in

---
+++ DIS-Journal/DIS-Journal/Logged.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Journal
+{
+    static class Logged
+    {
+        public static int Id { get; set; }
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+        public static string Email { get; set; }
+        public static string Password { get; set; }
+        public static DateTime Birth { get; set; }
+        public static string Role { get; set; }
+    }
+}
