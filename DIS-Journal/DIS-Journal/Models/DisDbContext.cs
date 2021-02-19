3665be2d087eb9db25f3459bf0c214c075e74849
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-02-19 15:34:11 +0200

    added comments to DisDbContext

--- DIS-Journal/DIS-Journal/Models/DisDbContext.cs
+++ DIS-Journal/DIS-Journal/Models/DisDbContext.cs
@@ -8,12 +8,15 @@ using MySql.EntityFrameworkCore;
 
 namespace DIS_Journal.Models
 {
+    //This is DbContext class, which is required for the connection between the database and the application
     class DisDbContext : DbContext
     {
+        //This collection referes to table 'users' (the name of the collection should be just like the name of the table which it refers to)
         public DbSet<User> users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //the connection string
             optionsBuilder.UseMySQL("server=bqa0euv4na39nvxw2o9w-mysql.services.clever-cloud.com;database=bqa0euv4na39nvxw2o9w;user=uvllsqwcceiys8wh;password=L3OZQwgPkyHILB6WkETs");
         }
     }
