4eacb9ec4adeb9448ac4750267afd92f1cbed412
Valeria <valeria.stamenova3@gmail.com>
2021-03-17 22:04:29 +0200

    g'night commit - units

---
+++ DIS-Journal/DIS-Journal.Tests/ScheduleTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+using DIS_Journal.Controllers;
+using DIS_Journal.Models;
+using DIS_Journal.Views;
+using System.Linq;
+
+namespace DIS_Journal.Tests
+{
+    [TestClass]
+    public class ScheduleTest
+    {
+        [TestMethod]
+        public void TestAddSubject()
+        {
+            string name = "Subject";
+            Color c = Color.AliceBlue;
+            ScheduleController.AddSubject(name, c);
+            Assert.IsTrue(ScheduleController.context.Subjects.Any(x => x.Title == name && Color.FromArgb(255, x.R, x.G, x.B) == c));
+        }
+    }
+}
